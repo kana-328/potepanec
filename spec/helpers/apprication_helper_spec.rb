3a002bebc4113b98ae2a6a20dff4b2a7c51baa29
波形佳奈 <namikatakana@namikatakananoMBP.local>
2020-05-22 18:00:44 +0900

    インデント、タイトル修正、不要なコメントアウト部分削除

--- spec/helpers/apprication_helper_spec.rb
+++ spec/helpers/apprication_helper_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 include ApplicationHelper
 RSpec.describe ApplicationHelper, type: :helper do
   describe 'ページタイトル' do
-    context '@product.nameがなかったら' do
-      it 'base_titleだけが表示される' do
+    context 'base_titleだけが表示される場合' do
+      it '@product.nameは空である' do
         expect(helper.full_title('')).to eq('BIGBAG Store')
       end
+      it '@product.nameはnilである' do
+        expect(helper.full_title(nil)).to eq('BIGBAG Store')
+      end
     end
-    context '@product.nameがあったら' do
-      it '@product.nameとbase_titleが表示される' do
+    context '@product.nameとbase_titleが表示される場合' do
+      it 'page_titleに@product.nameがある' do
         expect(helper.full_title('Tote')).to eq('Tote - BIGBAG Store')
       end
     end
