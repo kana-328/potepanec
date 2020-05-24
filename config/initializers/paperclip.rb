9b1922bf27ff356fa883a281c0cc17be575df33d
波形佳奈 <namikatakana@namikatakananoMBP.local>
2020-05-24 12:53:04 +0900

    paperclip編集

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,12 +1,12 @@
-if Rails.env.production?
+if ENV['AKIASYVPHMA4YLENABBQ']
   Paperclip::Attachment.default_options.merge!(
     storage: :fog,
     fog_credentials: {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-          region: ENV['AMAZON_S3_REGION'],
-          host: ENV['AMAZON_S3_BUCKET_NAME']
+      aws_access_key_id: ENV['AKIASYVPHMA4YLENABBQ'],
+      aws_secret_access_key: ENV['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'],
+          region: ENV['ap-northeast-1'],
+          host: ENV['potepanecnamikataec']
     },
         fog_directory: ENV["S3_BUCKET"]
   )
