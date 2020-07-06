baafced40fed58803243dd0ec5a864cabcd2dacd
波形佳奈 <namikatakana@namikatakananoMBP.local>
2020-07-06 22:05:17 +0900

    apiのgetリクエストlib配下に移した

--- spec/requests/potepan/suggest_spec.rb
+++ spec/requests/potepan/suggest_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe 'Potepan::Suggests', type: :request do
 
     it 'status:200がレスポンスで返ってきてbodyに期待する値が入っている' do
       get potepan_suggests_path params: { keyword: "r", max_num: 5 }
-      expect(response).to be_success
+      expect(response).to have_http_status(:success)
       expect(response.status).to eq 200
       expect(JSON.parse(response.body)).to eq ["ruby", "ruby for women", "ruby for men", "rails", "rails for women"]
     end
@@ -41,7 +41,7 @@ RSpec.describe 'Potepan::Suggests', type: :request do
 
     it "status:500がレスポンスで返ってくる" do
       get potepan_suggests_path params: { keyword: "r", max_num: 5 }
-      expect(response).not_to be_success
+      expect(response).not_to have_http_status(:success)
       expect(response.status).to eq 500
     end
   end
